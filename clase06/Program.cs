76f09f4e16efa155b733713ffc45d5acd1b22607
menendezSofia <sofimenendez_93@hotmail.com>
2022-06-07 19:52:26 -0300

    Agregar archivos de proyecto.

---
+++ clase06/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 
+suma de num
+mayor
+menor
+promedio
+mostrar lista
+mostrar cálculos
+*/
+
+int[]lista = new int[10];
+
+Console.WriteLine("Ingrese 10 números");
+for (int i = 0; i < 10; i++)
+{
+        lista[i] = int.Parse(Console.ReadLine());
+}
+
+// Cálculos
+int suma = lista.Sum();
+int mayor = lista.Max();
+int menor = lista.Min();
+double promedio = lista.Average();
+
+// Muestro resultados
+Console.WriteLine("Lista de números ingresados: ");
+foreach (var item in lista)
+{
+    Console.Write(item.ToString()+" ");
+}
+Console.Write("\nSumatoria: " + suma+
+              "\nMayor: "+ mayor+
+              "\nMenor: "+ menor+
+              "\nPromedio: "+promedio);
+
